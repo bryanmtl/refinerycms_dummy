446535bc48e84c672ebb4e00e03f448a1f75e652
Nicholas Henry <nicholas.henry@gmail.com>
2011-09-12 14:07:58 -0400

    Add base dummy test application for Refinery engines

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = 'ef351c21c55907817b986a400ba2df0f4e54979f2de4a973eb1f97e7ba3ffc7073a2f200ae8682dc8d1de7a9f82454d2501966bfec66f7a9c0ea52f7308fa112'
